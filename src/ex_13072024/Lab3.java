9fce7f80b73e8dbc2dba3f4edbf643889afd5ae3
lokesh1797 <lokeshkumar1797@gmail.com>
2025-05-18 16:13:16 +0530

    commit

---
+++ src/ex_13072024/Lab3.java
@@ -0,0 +1,19 @@
+package ex_13072024;
+
+public class Lab3 {
+    public static void main(String[] args) {
+        int age=87;
+        System.out.println("your age is "+age);
+
+        System.out.printf("your age is %d \n",age);
+
+        String s3="lokesh";
+        int x=99;
+        int y=100;
+
+        System.out.println(x+y+s3);
+        System.out.println(s3+x+y);
+        System.out.println(x+s3+y);
+
+    }
+}
---
+++ src/ex_13072024/lab2.java
@@ -0,0 +1,27 @@
+package ex_13072024;
+
+public class lab2 {
+    public static void main(String[] args) {
+
+        boolean is_married=true;
+        byte b=100;
+        short s=101;
+        int age=34;
+        long phonenumber= 9898789898L;
+        char grade='A';
+        float pi=3.14f;
+        double d=118.654;
+        String name="Lokesh";
+        System.out.println("byte="+b);
+        System.out.println("ismarried="+is_married);
+        System.out.println("short="+s);
+        System.out.println("age="+age);
+        //or System.out.printf("age= %d ",age);
+        System.out.println("phonenumber="+phonenumber);
+        System.out.println("grade="+grade);
+        System.out.println("pi="+pi);
+        System.out.println("double="+d);
+        System.out.println("String="+name);
+
+    }
+}
